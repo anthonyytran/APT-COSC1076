21c409b9c9d2f4d591edad43339db48eb33190dd
Anthony <anthony.tran99@hotmail.com>
2024-03-14 22:25:56 +1100

    Fixed code

--- Week01/circle.cpp
+++ Week01/circle.cpp
@@ -2,25 +2,28 @@
 
 #define PI 3.14159
 
-// Function that calculates the area of a circle
-double calculateArea(double radius)
-{
-    return PI * radius * radius;
-}
+// Function prototype
+double calculateArea(double radius);
 
 int main()
 {
     double radius;
 
-    // Prompt user to enter the radius
+    // Prompt the user to enter the radius
     std::cout << "Enter the radius of the circle: ";
     std::cin >> radius;
 
-    // Calculate area of circle using the function
+    // Calculate the area of the circle using the function
     double area = calculateArea(radius);
 
     // Print out the area of the circle
-    std::cout << "The area of the circle with radius " << radius << " is " << area << std::endl;
+    std::cout << "The area of the circle with radius " << radius << " is: " << area << std::endl;
 
     return 0;
+}
+
+// Function definition to calculate the area of the circle
+double calculateArea(double radius)
+{
+    return PI * radius * radius;
 }
