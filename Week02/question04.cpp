a85dece6e462feb9d920fdb404b5781128ed9a29
Anthony <anthony.tran99@hotmail.com>
2024-03-24 16:16:58 +1100

    Question 4a

---
+++ Week02/question04.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    char character;
+
+    // Prompt user to enter character
+    std::cout << "Enter a character: ";
+
+    // Read the character from input
+    std::cin >> character;
+
+    // Print the character
+    std::cout << "Character: " << character << std::endl;
+
+    return 0;
+}
